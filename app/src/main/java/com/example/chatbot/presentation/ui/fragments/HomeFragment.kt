83ec05c5e48fe319c13dcf7d4eefc241fa0a15a8
Chi Thien Ngo <125285789+ThienNg0@users.noreply.github.com>
2025-01-11 14:45:19 +0700

    home to chat

--- app/src/main/java/com/example/chatbot/presentation/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/chatbot/presentation/ui/fragments/HomeFragment.kt
@@ -1,8 +1,31 @@
 package com.example.chatbot.presentation.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.chatbot.R
+import com.example.chatbot.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lnCreateNewChats.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_chatFragment)
+        }
+    }
 
 }
