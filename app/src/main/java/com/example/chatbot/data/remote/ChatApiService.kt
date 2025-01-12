22fc70cbb9a759ceefcfa8c4d7bd3a3023e73579
Chi Thien Ngo <125285789+ThienNg0@users.noreply.github.com>
2025-01-12 18:56:37 +0700

    time history and delete

--- app/src/main/java/com/example/chatbot/data/remote/ChatApiService.kt
+++ app/src/main/java/com/example/chatbot/data/remote/ChatApiService.kt
@@ -32,7 +32,7 @@ class ChatApiServiceImpl @Inject constructor(
             }
 
             val request = Request.Builder()
-                .url("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=")
+                .url("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyA13-9MbJYX-eYcVCD0eIGLNynTcVm5EYs")
                 .post(RequestBody.create(JSON, jsonBody.toString()))
                 .build()
 
--- app/src/main/java/com/example/chatbot/presentation/adapters/ChatItemAdapter.kt
+++ app/src/main/java/com/example/chatbot/presentation/adapters/ChatItemAdapter.kt
@@ -4,16 +4,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatbot.R
 import com.example.chatbot.data.model.Chat
 
-class ChatItemAdapter(private val chatList: List<Chat>) :
+class ChatItemAdapter(private val chatList: List<Chat>,
+                      private val onDeleteChat: (String) -> Unit
+) :
     RecyclerView.Adapter<ChatItemAdapter.ChatItemViewHolder>() {
 
     inner class ChatItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.tv_tittle)
+        val deleteImageView: ImageView = itemView.findViewById(R.id.iv_delete)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatItemViewHolder {
@@ -25,6 +29,11 @@ class ChatItemAdapter(private val chatList: List<Chat>) :
         val chat = chatList[position]
         Log.d("ChatItemAdapter", "onBindViewHolder: position=$position, lastMessage=${chat.lastMessage}")
         holder.titleTextView.text = chat.lastMessage
+        holder.deleteImageView.setOnClickListener{
+            chat.chatId?.let { chatId ->
+                onDeleteChat(chatId)
+            }
+        }
     }
 
     override fun getItemCount() = chatList.size
