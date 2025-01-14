b24a52c8f7278cbb4ec22f2a2fb7bb825328ae04
Chi Thien Ngo <125285789+ThienNg0@users.noreply.github.com>
2025-01-14 14:56:01 +0700

    save user

--- app/src/main/java/com/example/chatbot/presentation/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/chatbot/presentation/viewmodels/HomeViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.chatbot.data.model.Chat
 import com.example.chatbot.data.model.ChatHistoryItem
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -16,7 +17,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val firestore: FirebaseFirestore
+    private val firestore: FirebaseFirestore,
+    private val firebaseAuth: FirebaseAuth
 ) : ViewModel() {
 
     private val _chatHistory = MutableLiveData<List<ChatHistoryItem>>(emptyList())
@@ -25,21 +27,23 @@ class HomeViewModel @Inject constructor(
     companion object {
         private const val TAG = "HomeViewModel"
     }
-
+    private val userUid = firebaseAuth.currentUser?.uid
     init {
         fetchChats()
     }
 
     private fun fetchChats() {
-        firestore.collection("chats")
-            .addSnapshotListener { querySnapshot, e ->
+        userUid?.let { firestore.collection("users").document(it)
+            .collection("chats")
+        }
+            ?.addSnapshotListener { querySnapshot, e ->
                 if (e != null) {
                     Log.e(TAG, "Listen failed.", e)
                     return@addSnapshotListener
                 }
                 val chats = querySnapshot?.documents?.mapNotNull { document ->
                     val chat = document.toObject(Chat::class.java)
-                    chat?.copy(chatId = document.id, timestamp = document.getTimestamp("timestamp")?.toDate() )
+                    chat?.copy(chatId = document.id)
                 } ?: emptyList()
 
                 val groupedChats = chats.groupBy { chat ->
@@ -56,11 +60,13 @@ class HomeViewModel @Inject constructor(
     }
     fun deleteChat(chatId: String) {
         viewModelScope.launch {
-            firestore.collection("chats").document(chatId).delete()
-                .addOnSuccessListener {
+            userUid?.let { firestore.collection("users").document(it)
+                .collection("chats").document(chatId)
+            }?.delete()
+                ?.addOnSuccessListener {
                     Log.d(TAG, "Chat deleted successfully with ID: $chatId")
                 }
-                .addOnFailureListener { e ->
+                ?.addOnFailureListener { e ->
                     Log.e(TAG, "Error deleting chat with ID: $chatId", e)
                 }
         }
